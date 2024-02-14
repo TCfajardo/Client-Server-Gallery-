414468769399777fcb8ddf42207264de66ff4c97
Catalina Fajardo <tania.fajardo01@uptc.edu.co>
2024-02-14 17:22:26 -0500

    acercamiento de la conexion cliente servidor

---
+++ src/infrastructure/Client.java
@@ -0,0 +1,29 @@
+package infrastructure;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class Client {
+    
+    public Client() {
+        final String host = "127.0.0.1";
+        final int port = 5000;
+
+        try {
+            Socket sc = new Socket(host, port);
+            System.out.println("Cliente conectado...");
+
+            sc.close();
+            
+            System.out.println("Cliente desconectado");
+            
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Client();
+    }
+}
